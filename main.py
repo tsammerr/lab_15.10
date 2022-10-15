31c1f531b54df6a270f2357d6f2f7583a77d4816
main <tarasovych.maria@gmail.com>
2022-10-15 10:07:51 +0300

    init: ex 1 f

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-x = int(input('i->'))
-y = int(input('j->'))
-
+x = int(input('side size -> '))
 for i in range(x):
-    for j in range(y):
-        if i >= j:
-           print('*', end=' ')
-        else:
-            print('*', end=' ')
+    for j in range(x):
+        print('*', end=' ')
     print()
