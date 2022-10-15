a11d775ac016f3300f1b2db54e226b778ae0d5ee
main <tarasovych.maria@gmail.com>
2022-10-15 10:41:26 +0300

    init: ex 3

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-x = int(input('side size-> '))
-
-for i in range(x):
-  for j in range(x):
-    print('*', end=' ')
-print()
+try:
+    x = int(input('side size -> '))
+    for i in range(x):
+        if j == 0 or j == x - 1:
+            print('  ', end=' ')
+        else:
+            print('  ', end=' ')
+        for in range(x):
+            if i == 0 or i == x - 1:
+                print('* ', end=' ')
+            else:
+                print('  ', end=' ')
+        print()
+except Exception as ex:
+    print(ex)
