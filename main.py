988189422afe42c0532abc0c38f7d3ef533ab141
main <tarasovych.maria@gmail.com>
2022-10-15 10:43:33 +0300

    init: ex 1 fix

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-x = int(input('side size -> '))
-for i in range(x):
-    for j in range(x):
-        print('*', end=' ')
-    print()
+try:
+    x = int(input('side size -> '))
+    for i in range(x):
+        for j in range(x):
+            print('*', end=' ')
+        print()
+except Exception as ex:
+    print(ex)
