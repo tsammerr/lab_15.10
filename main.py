b638a8e73deb2c425e369c28b971af9ab6132681
main <tarasovych.maria@gmail.com>
2022-10-15 10:42:06 +0300

    init: ex 3

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-x = int(input('height size-> '))
-y = int(input('width size-> '))
+try:
+    x = int(input('height size-> '))
+    y = int(input('width size-> '))
 
-for i in range(x):
-    for j in range(y):
-        if i >= j:
-           print('*', end=' ')
-        else:
-            print('*', end=' ')
-    print()
+    for i in range(x):
+        for j in range(y):
+            if i >= j:
+                print('*', end=' ')
+            else:
+                print('*', end=' ')
+        print()
